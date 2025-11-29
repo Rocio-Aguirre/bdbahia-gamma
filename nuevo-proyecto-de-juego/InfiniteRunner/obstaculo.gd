b3e3bdee51250584f511e77f419e5f2114c1ca67
diegoorbe96 <63675814+diegoorbe96@users.noreply.github.com>
2025-11-29 12:29:31 -0300

    player y obstaculo base

---
+++ nuevo-proyecto-de-juego/InfiniteRunner/obstaculo.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+@export var speed = 50
+
+
+func _physics_process(delta: float) -> void:
+	position.y += speed*delta
+
+
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
