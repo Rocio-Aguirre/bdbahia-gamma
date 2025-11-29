2e2b3a45b501749da7c4ecd2dc6c521a58d28b8f
diegoorbe96 <63675814+diegoorbe96@users.noreply.github.com>
2025-11-29 12:20:36 -0300

    Commit Inicial InfiniteRunner

---
+++ nuevo-proyecto-de-juego/InfiniteRunner/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@export var horizontal_move = 20.0
+
+func check_input():
+	var array = Input.get_axis("ui_left","ui_right")
+	return array
+
+func _physics_process(delta: float) -> void:
+	var direction = check_input()
+	velocity = Vector2.RIGHT*horizontal_move*direction
+	move_and_slide()
